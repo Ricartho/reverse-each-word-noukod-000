ae3c4ff7df086480c86a28bd87a771a8532a2747
Ricartho Franck <ricartho.franck@esih.edu>
2019-04-15 14:22:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -14,8 +14,8 @@ def reverse_each_word(st)
   new_array2 = []
   new_array = st.split
   
-  new_array.each do |el|
+  new_array.collect do |el|
     new_array2 << el.reverse
   end
-     return new_array2.join(" ")
+    new_array2.join(" ")
 end 
